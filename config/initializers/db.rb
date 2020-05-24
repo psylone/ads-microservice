b476bc178ed42814ca32a940087bc06770d267d6
Evgeniy Fateev <spaceflow@gmail.com>
2020-05-24 03:55:10 +0300

    feat(app): add PaginationLinks helper

--- config/initializers/db.rb
+++ config/initializers/db.rb
@@ -3,6 +3,8 @@
 
 Sequel.connect(Settings.db.to_hash)
 
+Sequel::Model.db.extension(:pagination)
+
 Sequel::Model.plugin :validation_helpers
 Sequel::Model.plugin :timestamps, update_on_create: true
 
