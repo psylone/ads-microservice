7c84e68fbc8bad5ace43a257564c6ed83f03cb56
Evgeniy Fateev <spaceflow@gmail.com>
2020-06-08 20:57:02 +0300

    feat(auth): move auth service url to settings

--- app/lib/auth_service/client.rb
+++ app/lib/auth_service/client.rb
@@ -6,7 +6,7 @@ module AuthService
     extend Dry::Initializer[undefined: false]
     include Api
 
-    option :url, default: proc { 'http://localhost:3010/v1' }
+    option :url, default: proc { Settings.service.auth.url }
     option :connection, default: proc { build_connection }
 
     private
