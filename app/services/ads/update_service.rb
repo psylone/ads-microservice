2f297c00f795881bc8f581b339118a49889bc05a
safrio <safrio@gmail.com>
2022-10-12 11:47:05 +0500

    remove extra

--- app/services/ads/update_service.rb
+++ app/services/ads/update_service.rb
@@ -11,10 +11,6 @@ module Ads
     option :ad, default: proc { Ad.first(id: @id) }
 
     def call
-      p '@@@'
-      p "id: #{@id}"
-      p "ad: #{@ad.inspect}"
-      p "data: #{@data.to_h}"
       return fail!(I18n.t(:not_found, scope: 'services.ads.update_service')) if @ad.blank?
       @ad.update_fields(@data.to_h.transform_keys(&:to_sym), %i[lat lon])
     end
